2a54fc8632bc7dac3cba541c748f168a41fd1a07
JhyunJ1 <jhyunj1005@gmail.com>
2023-01-30 13:41:38 +0900

    Solved class02 practice01

---
+++ Class_JS/class02/practice01.js
@@ -0,0 +1,29 @@
+function calculator(type, a, b) {
+	if (type === 'sum') {
+		return a + b
+	} else if (type === 'subtract') {
+		return a + b
+	} else {
+		alert('올바른 타입을 명시해주세요.')
+	}
+}
+
+let calculator = function(type, a, b) {
+    if (type === 'sum') {
+		return a + b
+	} else if (type === 'subtract') {
+		return a + b
+	} else {
+		alert('올바른 타입을 명시해주세요.')
+	}
+}
+
+let calculator = (type, a, b) => {
+    if (type === 'sum') {
+		return a + b
+	} else if (type === 'subtract') {
+		return a + b
+	} else {
+		alert('올바른 타입을 명시해주세요.')
+	}
+}
